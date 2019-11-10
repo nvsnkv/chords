44020a624d8a67af96f68fa3123b8169601bfdd5
[NV] Nikita Senkov <nvsnkv@live.com>
2019-11-10 19:17:37 +0300

    initial commit. Backend interface

---
+++ chords.server/Controllers/NotificationsController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Chords.Server.Model.Chords;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Chords.Server.Controllers
+{
+    [ApiController]
+    [Route("api/chords")]
+    public class ChordsController : ControllerBase
+    {
+        private readonly ILogger<ChordsController> _logger;
+
+        public ChordsController(ILogger<ChordsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Chord> GetChords()
+        {
+            return Enumerable.Empty<Chord>();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Chord GetChord(string id)
+        {
+            return null;
+        }
+
+        [HttpPut]
+        public void AddChord(Chord chord)
+        {
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public void DeleteChord(string id)
+        {
+        }
+
+        [HttpPost]
+        public void Touch(Chord chord)
+        {
+        }
+
+    }
+}
---
+++ chords.server/Model/Chords/Chord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chords.Server.Model.Chords
+{
+    public class Chord
+    {
+        public Guid Id { get; set; }
+
+        public string Payload { get; set; }
+
+        public DateTime? TouchedOn { get; set; }
+    }
+}
