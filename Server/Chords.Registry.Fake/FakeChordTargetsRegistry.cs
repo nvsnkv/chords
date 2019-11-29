fb7b295b1ab1e602617c3c18a044d15d42575a61
[NV] Nikita Senkov <nvsnkv@live.com>
2019-11-29 23:14:58 +0300

    added fake registries

---
+++ Server/Chords.Registry.Fake/FakeChordTargetsRegistry.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Chords.Domain.Contract.Data;
+using Chords.Domain.Contract.Registry;
+
+namespace Chords.Registry.Fake
+{
+    public sealed class FakeChordTargetsRegistry : IChordTargetsRegistry
+    {
+        public IReadOnlyList<IChordTarget> GetItems(IUser owner)
+        {
+            return new List<IChordTarget>().AsReadOnly();
+        }
+
+        public IChordTarget Get(IUniqueItem item, IUser owner)
+        {
+            throw new ArgumentException("Unable to find requested item: fake registry is always empty!", nameof(item));
+        }
+
+        public bool Exists(IUniqueItem item, IUser owner)
+        {
+            return false;
+        }
+
+        public void Update(IChordTarget item, IUser owner)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IUniqueItem Add(IChordTarget item, IUser owner)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(IUniqueItem item, IUser owner)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IReadOnlyList<IChordTarget> GetItemsByChord(IChord chord)
+        {
+            throw new ArgumentException("Unable to find requested items: fake registry is always empty!", nameof(chord));
+        }
+    }
+}
---
+++ Server/Chords.Registry.Fake/FakeChordsRegistry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Chords.Domain.Contract.Data;
+using Chords.Domain.Contract.Registry;
+
+namespace Chords.Registry.Fake
+{
+    public sealed class FakeChordsRegistry : IChordsRegistry
+    {
+        public IReadOnlyList<IChord> GetItems(IUser owner)
+        {
+            return new List<IChord>().AsReadOnly();
+        }
+
+        public IChord Get(IUniqueItem item, IUser owner)
+        {
+            throw new ArgumentException("Unable to find requested item: fake registry is always empty!", nameof(item));
+        }
+
+        public bool Exists(IUniqueItem item, IUser owner)
+        {
+            return false;
+        }
+
+        public void Update(IChord item, IUser owner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IUniqueItem Add(IChord item, IUser owner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IUniqueItem item, IUser owner)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
